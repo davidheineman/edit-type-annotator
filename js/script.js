d886d537b844d7075da0c57ed6d9e870ef0531cb
David <dheineman3@gatech.edu>
2021-10-13 23:15:29 -0400

    minor changes

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ function generateView(sent) {
 }
 
 function createGroup(df, container_id) {
-    for (var i = 0; i < df.length; i++) {
+    for (let i = 0; i < df.length; i++) {
         // Write sentence
         let s = df[i][0];           // sentence
         let contr = "<p>";          // DOM container for sent
@@ -36,7 +36,7 @@ function createGroup(df, container_id) {
         contr = contr.concat(s.substring(0, df[i][1][1]));
 
         // Show edits
-        for (var j = 1; j < df[i].length; j++) {
+        for (let j = 1; j < df[i].length; j++) {
             let edit = df[i][j];
 
             // Add the non-annotated part of the sentence
@@ -65,16 +65,123 @@ function createGroup(df, container_id) {
         contr = contr.concat("</p>");
 
         // Create col container for sentence
-        let box = '<input type="text" class="form-control" aria-label="Score">'
+        let box = '<input min="0" max="100" class="form-control" aria-label="Score">'
         let div = "<div class='row'><div class='col-2'>" + box + "</div><div class='col-10'>" + contr + "</div></div>";
         let li = $("<li class='list-group-item'></li>").append($(div));
-        
+
         $(container_id).append(li);
+
+        disableFormControl();
     }
     $(container_id).sortable().disableSelection();  
+}
+
+function disableFormControl() {
+    $('.form-control').bind('keypress', function(e) {
+        if (!(e.keyCode > 47 && e.keyCode < 58)) {
+            // Check if the new char is non-numeric
+            e.preventDefault();
+        } else {
+            // Check if the new value is out of bounds
+            let nv = parseInt($(this).val() + String.fromCharCode(e.keyCode));
+            if (nv > 100) {
+                e.preventDefault();
+                // add warning pop-up
+                console.log('Out of bounds!')
+            } else if (nv < 0) {
+                // Don't need warning pop-up because less likely
+                console.log('Negative!')
+            }
+        }
+    })
+}
+
+var out  = [];
+
+function submitForm() {
+    let sent_out = {};
+
+    sent_out['ID'] = parseInt((new URLSearchParams(window.location.search)).get('s'));
+    sent_out['Original'] = $('#input-sent-above').text();
+    sent_out['Deletions'] = parseSentList("#del-list");
+    sent_out['Paraphrases'] = parseSentList("#par-list");
+    sent_out['Splittings'] = parseSentList("#spt-list");
+
+    out = out.concat(sent_out);
+    downloadData(out);    
+}
+
+function parseSentList(container_id) {
+    let out = [];
+    $(container_id + " .list-group-item").each(function(i) {
+        let entry = [];
+
+        // get value of input
+        entry.push(parseInt($($($($(this).children()[0]).children()[0]).children()[0]).val()));
+
+        // get sentence
+        entry.push($($($($(this).children()[0]).children()[1]).children()[0]).text());
+
+        // get complete html
+        let html = $($($($(this).children()[0]).children()[1]).children()[0]).html();
+
+        // Parse edits from sentences
+        let idx = 0;
+        while (idx < html.length) {
+            let next_substring = html.substring(idx, idx + 18);
+            if (next_substring == '<span class="del">') {
+                html = html.substring(0, idx) + html.substring(idx + 25);
+                entry.push([0, idx, idx])
+            } else if (next_substring == '<span class="spt">') {
+                html = html.substring(0, idx) + html.substring(idx + 25);
+                entry.push([2, idx, idx])
+            } else if (next_substring == '<span class="par">') {
+                // This one is different because there contains text in the span
+                html = html.substring(0, idx) + html.substring(idx + 18);
+                let found = false;
+                let jdx = 0;
+                while (!found) {
+                    if (html.substring(jdx, jdx + 7) == '</span>') {
+                        html = html.substring(0, jdx) + html.substring(jdx + 7);
+                        found = true;
+                    } else {
+                        jdx++;
+                    }
+                }
+                entry.push([1, idx, jdx])
+            } else {
+                idx++;
+            }
+        }
+        out.push(entry);
+    })
+    return out;
+}
+
+
+// TODO: 
+// [X] Handle form submission (get sentences, scores, package into json and download)
+// [X] Prevent non-numeric or out of bounds inputs
+// [ ] Make much prettier
+// [ ] Add "instructions" button with pop-up hover thing
+// [ ] Add ability to handle multiple sentences
+// [ ] Add pop-up warning for out-of-bounds inputs
+
+function downloadData(data) {
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", "output.json");
+    document.body.appendChild(downloadAnchorNode);
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
 }
 
 $.ajax({
     url: 'data/input.json',
     dataType: 'json',
 }).done(displayAnnotator);
+
+$('button#submit').on('click', function() {
+    submitForm();
+})
